2778ac544bbe3db7f309a16e80d221b2184ccfbd
Yohann <yohannassouline@hotmail.fr>
2020-01-09 23:14:06 +0100

    add a script to easily install my vim conf

---
+++ vim/install_my_conf.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# stop at first error
+set -e
+
+mkdir .config/nvim/
+
+# mon vimrc
+wget -O ~/.vimrc/ https://raw.githubusercontent.com/Yohannfra/dotfiles/master/vim/.vimrc
+
+# coc-settings
+wget -O ~/.config/nvim/coc-settings.json https://raw.githubusercontent.com/Yohannfra/dotfiles/master/vim/coc-settings.json
+
+# init.vim
+wget -O ~/.config/nvim/init.vim https://raw.githubusercontent.com/Yohannfra/dotfiles/master/vim/init.vim
+
+# install Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install all plug plugins
+vim -c "PlugInstall" -c qa
+# install coc-python (lsp python)
+vim -c "CocInstall coc-python" -c qa
+
+# tools for python
+pip install python-language-server --user
+pip install jedi --user
+
+echo -e "\nDone!"
+echo "You should install clang and clang extra tools if you want to have the autocompletion and lsp features in c/c++"
